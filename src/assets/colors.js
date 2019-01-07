ecb112e3319af0ebd757bc6185b48abb80c1af7a
salvar1able <salvadorbsx@gmail.com>
2019-01-06 22:54:03 -0600

    Add project colors and container styles

---
+++ src/assets/colors.js
@@ -0,0 +1,10 @@
+
+const OdinTheme = {
+    horizon: "#485675",
+    bliss: "#6184B4",
+    fog: "#D5CDC7",
+    stone: "#BFAC98",
+    clay: "#DC7A55"
+};
+
+export default OdinTheme;
---
+++ src/assets/styles.js
@@ -0,0 +1,14 @@
+import OdinTheme from "./colors";
+
+const styles = {
+    container: {
+        height: "100vh",
+        backgroundColor:
+        OdinTheme.fog,
+        padding: 8,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+};
+
+export default styles;
