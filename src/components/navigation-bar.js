330046e7cbf3621fe60118d6d790d3b7049b2368
salvar1able <salvadorbsx@gmail.com>
2019-02-01 16:37:48 -0600

    Add toggle on NavBar for different evaluating profiles

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -4,11 +4,11 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 const styles = {
     root: {
@@ -23,22 +23,48 @@ const styles = {
     },
 };
 
-function ButtonAppBar(props, state) {
-    const { classes } = props;
-    const { auth, anchorEl } = state;
-    const open = Boolean(anchorEl);
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" color="inherit" className={classes.grow}>
-                        ODIN v0.1
-                    </Typography>
-                    {auth && (
-                        <div>
+class MenuAppBar extends React.Component {
+    state = {
+        anchorEl: null,
+        selectedRole: null
+    };
+
+    handleMenu = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+    handleProfessor = () => {
+        localStorage.setItem("staffLogged", "Professor");
+        this.setState({ selectedRole: "Professor" });
+    };
+    handleMentor = () => {
+        localStorage.setItem("staffLogged", "Mentor");
+        this.setState({ selectedRole: "Mentor" });
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { anchorEl, selectedRole } = this.state;
+        const open = Boolean(anchorEl);
+        const menuLabel = (selectedRole === null ) ? "stranger" : selectedRole;
+        const staffMember = (localStorage.getItem("staffLogged"));
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            Odin
+                        </Typography>
+                        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                            <Typography variant="h6" color="inherit" className={classes.grow}>
+                                Hi, { (staffMember) ? staffMember : menuLabel }!
+                            </Typography>
                             <IconButton
                                 aria-owns={open ? 'menu-appbar' : undefined}
                                 aria-haspopup="true"
@@ -61,19 +87,19 @@ function ButtonAppBar(props, state) {
                                 open={open}
                                 onClose={this.handleClose}
                             >
-                                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                                <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                                <MenuItem onClick={this.handleProfessor}>Professor</MenuItem>
+                                <MenuItem onClick={this.handleMentor}>Mentor</MenuItem>
                             </Menu>
                         </div>
-                    )}
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
 }
 
-ButtonAppBar.propTypes = {
+MenuAppBar.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ButtonAppBar);
+export default withStyles(styles)(MenuAppBar);
